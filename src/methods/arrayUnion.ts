efc1f2355576a1352b57b7b972e6488026b78c6b
Germán Aliprandi <galiprandi@gmail.com>
2025-02-24 23:18:42 -0300

    refactor: enhance arrayUnion function with input validation and improved performance

--- src/methods/arrayUnion.ts
+++ src/methods/arrayUnion.ts
@@ -1,10 +1,14 @@
 /**
- * Returns a new array containing all unique elements from both arrays.
+ * Returns the union of two arrays (all unique elements from both arrays).
  * @param {T[]} array1 - The first array.
  * @param {T[]} array2 - The second array.
- * @returns {T[]} A new array containing unique elements from `array1` and `array2`.
+ * @returns {T[]} A new array containing all unique elements from both arrays.
  * @template T
  */
 export function arrayUnion<T>(array1: T[], array2: T[]): T[] {
-    return [...new Set([...array1, ...array2])];
+    if (!Array.isArray(array1) || !Array.isArray(array2)) {
+        throw new Error("Both arguments must be arrays");
+    }
+
+    return Array.from(new Set([...array1, ...array2]));
 }
